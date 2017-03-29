08bb833e48d09d0517e3499084723b2420d962ca
BenJohnCarson <benjohncarson@gmail.com>
2017-03-29 12:33:00 +0100

    all working

--- clock/src/index.js
+++ clock/src/index.js
@@ -1,16 +1,23 @@
 "use strict";
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function setDate() {
+function updateTime() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    moveHand(now.getSeconds(), secondHand);
+    moveHand(now.getMinutes(), minuteHand);
+    moveHand(now.getHours(), hourHand, true);
 }
 
-function setSeconds() {
-    
+function moveHand(time, element, isHour = false) {
+    const degrees = setDegrees(time, isHour);
+    element.style.transform = `rotate(${degrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+function setDegrees(time, isHour) {
+    const divisor = isHour ? 12 : 60;
+    return ((time / divisor) * 360) + 90;
+}
+
+setInterval(updateTime, 1000);
