0a13d5d2662fd363e55115c50feb6a7c63e109c6
BenJohnCarson <benjohncarson@gmail.com>
2017-04-19 16:10:15 +0100

    slight refactor, doesn't polute global and reads easier

--- html5-canvas/src/index.js
+++ html5-canvas/src/index.js
@@ -1,47 +1,63 @@
-const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.width = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 100;
-ctx.globalCompositeOperation = 'subtract';
+{
+  const canvas = document.querySelector('#draw');
+  const ctx = canvas.getContext('2d');
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-let hue = 0;
-let direction = true;
+  canvas.width = window.innerWidth;
+  canvas.width = window.innerHeight;
 
-function draw(e) {
-  if (!isDrawing) return;
-  console.log(e);
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY]
-  hue++;
-  if(hue >= 360) {
-    hue = 0;
+  ctx.strokeStyle = '#BADA55';
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  ctx.lineWidth = 100;
+  ctx.globalCompositeOperation = 'subtract';
+
+  {
+    canvas.addEventListener('mousedown', (e) => {
+      isDrawing = true;
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
   }
-  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
+
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+  let hue = 0;
+  let direction = true;
+
+  function draw(e) {
+    if (!isDrawing) return;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    calculateDraw(e);
+    changeHue();
+    changeLineWidth();
   }
-  if (direction) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
+
+  function calculateDraw(e) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY]
+  }
+
+  function changeHue() {
+    hue++;
+    if(hue >= 360) {
+      hue = 0;
+    }
+  }
+
+  function changeLineWidth() {
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+    if (direction) {
+      ctx.lineWidth++;
+    } else {
+      ctx.lineWidth--;
+    }
   }
 }
-
-canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
