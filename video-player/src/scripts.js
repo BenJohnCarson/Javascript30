2dd0227003d4629614815cac124d260550665d88
BenJohnCarson <benjohncarson@gmail.com>
2017-04-27 16:01:36 +0100

    fullscreen button working

--- video-player/src/scripts.js
+++ video-player/src/scripts.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fullscreen')
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
@@ -34,6 +35,10 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  document.webkitFullscreenElement ? document.webkitExitFullscreen() : player.webkitRequestFullscreen();
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -51,3 +56,6 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+
+fullscreenButton.addEventListener('click', toggleFullscreen);
